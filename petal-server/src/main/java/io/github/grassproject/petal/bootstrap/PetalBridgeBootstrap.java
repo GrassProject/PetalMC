cc18365fddbd284294e2308ebb298908ac42e8bb
ASTAR <apo2073@outlook.com>
2026-01-25 22:15:32 +0900

    feat: fabric

--- petal-server/src/main/java/io/github/grassproject/petal/bootstrap/PetalBridgeBootstrap.java
+++ petal-server/src/main/java/io/github/grassproject/petal/bootstrap/PetalBridgeBootstrap.java
@@ -1,6 +1,8 @@
 package io.github.grassproject.petal.bootstrap;
 
+import net.fabricmc.loader.impl.game.GameProvider;
 import net.fabricmc.loader.impl.game.minecraft.MinecraftGameProvider;
+import net.fabricmc.loader.impl.game.patch.GameTransformer;
 import net.fabricmc.loader.impl.launch.FabricLauncher;
 import net.fabricmc.loader.impl.util.Arguments;
 
@@ -8,63 +10,95 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
-public class PetalBridgeBootstrap extends MinecraftGameProvider {
-    private Path modFile;
-
-    @Override
-    public void initialize(FabricLauncher launcher) {
-        System.out.println("\u001B[31mFarbicProvider initialize");
-        System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
-        System.setProperty("log4j.jul.LoggerAdapter", "io.izzel.arclight.boot.log.ArclightLoggerAdapter");
-        System.setProperty("log4j.configurationFile", "arclight-log4j2.xml");
-        try {
-            this.modFile = this.extract();
-            launcher.addToClassPath(modFile);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        for (var lib : System.getProperty("petal.fabric.classpath").split(File.pathSeparator)) {
-            launcher.addToClassPath(Paths.get(lib));
-        }
-        super.initialize(launcher);
-    }
-
-    @Override
-    public Arguments getArguments() {
-        Arguments arguments = super.getArguments();
-        String old = arguments.get(Arguments.ADD_MODS);
-        var builtinMods = System.getProperty("arclight.fabric.builtinMods");
-        var path = this.modFile.toString() + File.pathSeparator + builtinMods;
-        if (old != null) {
-            path = old + File.pathSeparator + path;
-        }
-        arguments.put(Arguments.ADD_MODS, path);
-        return arguments;
-    }
-
-    private Path extract() throws Exception {
-        String version = "1.0.0";
-        System.setProperty("petal.version", version);
-        var path = getClass().getModule().getResourceAsStream("/common.jar");
-        var dir = Paths.get(".arclight", "mod_file");
-        if (!Files.exists(dir)) {
-            Files.createDirectories(dir);
-        }
-        var mod = dir.resolve(version + ".jar");
-        if (!Files.exists(mod) || Boolean.getBoolean("arclight.alwaysExtract")) {
-            try (var files = Files.list(dir)) {
-                for (Path old : files.toList()) {
-                    Files.delete(old);
-                }
-                Files.copy(path, mod);
-            }
-        }
-        return mod;
-    }
+public class PetalBridgeBootstrap implements GameProvider {
 
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public boolean locateGame(FabricLauncher launcher, String[] args) {
+        return true;
+    }
+
+    @Override
+    public void initialize(FabricLauncher launcher) {
+        System.out.println("Petal GameProvider initialize");
+    }
+
+    @Override
+    public GameTransformer getEntrypointTransformer() {
+        return null;
+    }
+
+    @Override
+    public void unlockClassPath(FabricLauncher launcher) {
+
+    }
+
+    @Override
+    public void launch(ClassLoader loader) {
+
+    }
+
+    @Override
+    public String getGameId() {
+        return "petalmc";
+    }
+
+    @Override
+    public String getGameName() {
+        return "PetalMC";
+    }
+
+    @Override
+    public String getRawGameVersion() {
+        return "1.21.8";
+    }
+
+    @Override
+    public String getNormalizedGameVersion() {
+        return getRawGameVersion();
+    }
+
+    @Override
+    public Collection<BuiltinMod> getBuiltinMods() {
+        return List.of();
+    }
+
+    @Override
+    public String getEntrypoint() {
+        return "org.bukkit.craftbukkit.Main";
+    }
+
+    @Override
+    public Path getLaunchDirectory() {
+        return Paths.get(".");
+    }
+
+    @Override
+    public boolean requiresUrlClassLoader() {
+        return false;
+    }
+
+    @Override
+    public Set<BuiltinTransform> getBuiltinTransforms(String className) {
+        return Set.of();
+    }
+
+    @Override
+    public Arguments getArguments() {
+        return new Arguments();
+    }
+
+
+    @Override
+    public String[] getLaunchArguments(boolean sanitize) {
+        return new String[0];
     }
 }
